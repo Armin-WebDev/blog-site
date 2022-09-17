0490ed22ffd8c3c12ba8ad3b81a2bebfa1ec2599
ArminP <ArminPouralvani@outlook.com>
2022-09-17 12:51:29 +0430

    creating another model named UserProfile

--- website/models.py
+++ website/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -12,3 +12,19 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)
     #category 
+    class Meta:
+        ordering =  ['-created_at']
+
+    def __str__(self) :
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='media')
+    description = models.CharField(max_length=320)
+
+    def ___str__(self):
+        return self.first_name + " " + self.last_name
+
+
+
