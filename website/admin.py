cf5828b27374dea1bb9a7d3b11b0e84b55d56652
ArminP <ArminPouralvani@outlook.com>
2022-09-17 12:52:38 +0430

    Registering and customizing models in admin

--- website/admin.py
+++ website/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','created_at','status')
+    search_fields = ['title','content']
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['first_name','email','date_joined']
